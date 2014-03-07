8e25144ae20172f7fda8476ba8b114499962317c
Tyler <zempter@gmail.com>
2014-03-07 11:27:47 -0700

    Edited Javascript
    Made javascript a little smarter when hiding divs.

--- javafunctions.php
+++ javafunctions.php
@@ -17,11 +17,18 @@
 		document.getElementById('selectedImage').innerHTML=document.getElementById(photoId).innerHTML
 	}
 	
-	function hide_image_and_comments(){
-		document.getElementById('image_and_comments').style.display = 'none';
+	function hide_element(elm){
+		var element = document.getElementById(elm);
+		if(typeof(element) != 'undefined' && element != null){
+			element.style.display='none';
+		}
+	}
+	
+	function hide_all(){
+		hide_element('image_and_comments');
 		document.getElementById('shadow').style.display = 'none';
 	}
 </script>
 
 
-<div id='shadow' onclick='hide_image_and_comments()'></div>
+<div id='shadow' onclick='hide_all()'></div>
