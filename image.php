b9dddacfc3ea9a1ab461527d4003f729bc8488d8
Tyler <zempter@gmail.com>
2014-03-17 20:53:03 -0600

    image.php security added, and code removed that broke stuff.

--- image.php
+++ image.php
@@ -1,12 +1,11 @@
 <?php
+include_once("secure.php");
 include_once("functions.php");
 	$data = $_GET['imageID'];
 	$query = "select data from images where imageID = '$data'";
 	$results = dbGet($query);
 	$data = mysql_fetch_row($results);
 	header("content-type: image/jpeg");
-	header('Cache-Control: private');
-	header("filename='$_GET[imageID]'");
 	echo $data[0];
 
 
