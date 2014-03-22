a31987eb12eacd1512c724984bc16fdb22e8731f
Tyler <zempter@gmail.com>
2014-03-21 21:43:56 -0600

    Image Cache theoretically works.
    From the data transfer i see in chrome, it looks like it is caching. I
    checked in firefox and explorer too, they seem to have low data
    transfers afterwards.

--- image.php
+++ image.php
@@ -5,8 +5,26 @@ include_once("functions.php");
 	$query = "select data from images where imageID = '$data'";
 	$results = dbGet($query);
 	$data = mysql_fetch_row($results);
-	header("content-type: image/jpeg");
-	echo $data[0];
+	$ETAG = "896433576890$_GET[imageID]";
+	//Header stuff
+	
+	
+	if(isset($_SERVER['HTTP_IF_NONE_MATCH']) AND $_SERVER['HTTP_IF_NONE_MATCH'] == $ETAG){
+		header('HTTP/1.1 304 Not Modified'); 
+        exit(0); 
+	}
+	else{
+		session_cache_limiter('public');
+		header("content-type: image/jpeg");
+		header("ETag: $ETAG"); 
+		define("MAXSECONDS",36000);//5 MINUTES
+		header_remove("Cache-Control");
+		header_remove("Pragma");
+		header_remove("X-Powered-By");
+		header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time()+(MAXSECONDS)));  
+		echo $data[0];
+	}
+	
 
 
 ?>
